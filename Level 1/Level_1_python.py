9ef5a07c82202c97faa9b1ab33becf924c622fad
Sl33pyCl0ud <julien.hassoun@efrei.net>
2022-09-29 15:52:41 +0200

    remplissage de la database

--- Level 1/Level_1_python.py
+++ Level 1/Level_1_python.py
@@ -5,19 +5,16 @@ import sqlite3
 conn = sqlite3.connect('ma_base.db')
 
 cursor = conn.cursor()
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS users(
-     id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
-     name TEXT,
-)
-""")
+cursor.execute("""DROP TABLE IF EXISTS users""")
+cursor.execute("""DROP TABLE IF EXISTS deals""")
+cursor.execute("""CREATE TABLE IF NOT EXISTS  users (id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE, name TEXT)""")
+cursor.execute("""CREATE TABLE IF NOT EXISTS  deals (id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE, amount INTEGER, user_id INTEGER)""")
 conn.commit()
 
-users = []
-users.append(("Nicolas"))
-users.append(("Math"))
-cursor.executemany("""
-INSERT INTO users(name) VALUES(?, ?)""", users)
+name1 = "Nicolas"
+name2 = "Math"
+cursor.execute("""INSERT INTO users(name) VALUES(?)""", (name1,))
+cursor.execute("""INSERT INTO users(name) VALUES(?)""", (name2,))
 conn.commit()
 
 deals = []
@@ -31,9 +28,13 @@ cursor.executemany("""
 INSERT INTO deals(amount, user_id) VALUES(?, ?)""", deals)
 conn.commit()
 
-cursor.execute("""SELECT name FROM users""")
-user1 = cursor.fetchone()
-print(user1)
+cursor.execute("""SELECT amount, user_id FROM deals""")
+deals_print = cursor.fetchall()
+print(deals_print)
+
+cursor.execute("""SELECT COUNT() FROM deals where user_id == 1""")
+count = cursor.fetchone()[0]
+print(count)
 
 
 conn.close()
